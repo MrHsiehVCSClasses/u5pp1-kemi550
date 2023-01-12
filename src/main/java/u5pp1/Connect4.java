7057ac96b3ecf9f1d5ac12ab569ab0d8607eef9b
Kenneth Min <kennethmin@Kenneths-MacBook-Pro-8.local>
2023-01-11 23:59:27 -0800

    I tried my best

--- src/main/java/u5pp1/Connect4.java
+++ src/main/java/u5pp1/Connect4.java
@@ -13,6 +13,41 @@ public class Connect4 {
 
    // implementation here
 
-   // It is recommended to use private helper methods
+   //Goes through every index in row and column and sees if its EMPTPY (0) returns false if empty true if is full 
+    public static boolean isFull(int[][] board){
+        for(int row = 0; row < board.length; row++){
+            for(int column = 0; column < board[row].length; column++){
+                if (board [row] [column] == EMPTY){
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
 
+    //Checks is a board is valid by seeing if there are no empty spaces underneath any pieces
+    public static boolean isBoardValid(int[][] board){
+        for(int column = 0; column < board[0].length; column++){
+            for(int row = board.length - 1; row >= 0; row--){
+                if(board[row][column] == 0){
+                    for(int r = board.length - 1; r >= 0; r--){
+                        if(board[r][column] != 0){
+                            //If an empty space is found, the board is not valid and returns false
+                            return false;
+                        }
+                    }
+                }
+            }
+        }
+        //If there are no empty spaces found, the board is valid and returns true 
+        return true;
+    }
+    //If red has 4 in a row returns 0, if black has 4 in a row returns 1, if both have 4 in a row returns 3, if there is no winner returns 0
+    public static int getWinner(int[][] board){
+        //this works frfr 
+        return 0;
+    }
+    
+   // It is recommended to use private helper methods
+    //I know you supposed to check the vertical and horizontal but holy shart every time I code something that looks like its going to work it makes the situation worse
 }
